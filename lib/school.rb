115b28d4641e862fe52cb7fedb07377dc831423c
Anthony Mai <anthony.mai530@gmail.com>
2019-11-24 02:39:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+  attr_reader :student
+  def initialize(name)
+    @name = name
+    @roster = {}
+    def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
+end
+end
